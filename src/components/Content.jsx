98a249e5ba43a176bef73f8f608e00850ad16d34
John Paul Inhog <jpinhog@indracompany.com>
2021-03-16 13:17:47 +0800

    Initital commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+    return (
+        <div>
+           <section id="about">
+                <div className="information">
+                    <h1>
+                        John Paul Inhog
+                    </h1>
+                    <p>Pasig City, 1600 - 09177388097</p>
+                </div>
+                <div className="details">
+                    <p>Hi, there! I am John Paul. I am Full stack developer with 4+ years experience developing web applications using agreed tools, libraries and coding standard. </p>
+                </div>
+           </section>
+           <section id="experience">
+                experiences
+           </section>
+           <section id="education">
+                Education
+           </section>
+           <section id="skills">
+                Skills
+           </section>
+           <section id="interests">
+                intereset
+           </section>
+           <section id="awards">
+                awards
+           </section>
+        </div>
+    )
+}
+ 
+export default Content;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import imageUrl from '../assets/images/user-logo.jpg';
+
+const Sidebar = () => {
+
+    const scrollLink = (e) => {
+        const navLinks = Array.from(document.getElementsByClassName('nav-link'));
+        navLinks.forEach(element => {
+            element.classList.remove('active');
+        });
+        e.currentTarget.children[0].classList.add('active');
+        const linkId = e.currentTarget.dataset.id;
+        document.getElementById(linkId).scrollIntoView({block: 'start', behavior: 'smooth' });
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="sideNav">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                <span className="d-block d-lg-none">John Paul Inhog</span>
+                <span className="d-none d-lg-block">
+                    <img className="img-fluid img-profile" src={imageUrl} alt="UserImage"/>
+                </span>
+            </a>
+            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <li className="nav-item" onClick={scrollLink} data-id="about"><a className="nav-link active" >About</a></li>
+                    <li className="nav-item" onClick={scrollLink} data-id="experience"><a className="nav-link" >Experience</a></li>
+                    <li className="nav-item" onClick={scrollLink} data-id="education"><a className="nav-link" >Education</a></li>
+                    <li className="nav-item" onClick={scrollLink} data-id="skills"><a className="nav-link" >Skills</a></li>
+                    <li className="nav-item" onClick={scrollLink} data-id="interests"><a className="nav-link" >Interests</a></li>
+                    <li className="nav-item" onClick={scrollLink} data-id="awards"><a className="nav-link" >Awards</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+ 
+export default Sidebar;
